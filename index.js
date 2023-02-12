4c5f0ccf637db2b5dabfc37c803c12fb2a8d732c
Arthur <truong.nguyen2@sotatek.com>
2023-02-12 13:11:06 +0700

    resolve conflict

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const path = require("path");
 
-<<<<<<< HEAD
-console.log('Lavilo');
-console.log('Lavilo ver 2');
-console.log('asdjasdhjasdhajshdhj');
-=======
 console.log("Lavilo");
 console.log("Lavilo ver 2");
 console.log("Lavilo ver 3");
 console.log("Lvadiasd");
->>>>>>> feat/102
--- index_ver2.js
+++ index_ver2.js
@@ -1,12 +1,5 @@
-const fs = require('fs');
+const fs = require("fs");
 
+fs.read("dump.txt", async () => {});
 
-fs.read("dump.txt", async () => {
-
-})
-
-<<<<<<< HEAD
-console.log("Another log in index_ver2.js file !!! main branch !");
-=======
 console.log("Another log in index_ver2.js file !!! feat/102");
->>>>>>> feat/102
