014204adc9b3fd19eca4303234d8a327f6187df0
Arthur <truong.nguyen2@sotatek.com>
2023-02-12 12:54:24 +0700

    Commit

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const path = require('path');
 
 console.log('Lavilo');
 console.log('Lavilo ver 2');
+<<<<<<< Updated upstream
+=======
+console.log('asdjasdhjasdhajshdhj');
+console.log("Revert");
+>>>>>>> Stashed changes
