70b343428485c2577bca386dc0c44b4b9cec74ff
Arthur <truong.nguyen2@sotatek.com>
2023-02-12 12:56:01 +0700

    Resolve conflict

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const path = require('path');
+const path = require("path");
 
-console.log('Lavilo');
-console.log('Lavilo ver 2');
-<<<<<<< Updated upstream
-=======
-console.log('asdjasdhjasdhajshdhj');
+console.log("Lavilo");
+console.log("Lavilo ver 2");
+console.log("asdjasdhjasdhajshdhj");
 console.log("Revert");
->>>>>>> Stashed changes
